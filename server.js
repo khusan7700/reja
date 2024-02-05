488f417fa7e583a41cce1f115f0129f3bdd818f9
Khusan <khusan7700@gmail.com>
2024-02-05 09:48:03 +0500

    fixed express

--- server.js
+++ server.js
@@ -12,25 +12,27 @@ app.use(express.urlencoded({extended: true}));
 
 // 2 Session code
 
-// 3 code
+// 3 Views code
 //first neet to install EJS
 app.set("views", "views");
 app.set("view engine", "ejs");
 // ejs orqali backend da fontend(HTML)ni yasaymiz.
 
 // 4 Ruting code
-// app.get("/hello", function(req, res){
-//     res.end(`<h1>HELLO WORLD</h1>`);
-// });
+app.get("/hello", function(req, res){
+    res.end(`<h1>HELLO WORLD</h1>`);
+});
 
-// app.get("/gift", function(req, res){
-//     res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
-// });
+app.get("/gift", function(req, res){
+    res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
+});
+
 
 app.post("/create-item", (req, res) => {
     console.log(req.body);
-    res.json({test: "seccess"});
+    res.json({test: "seccess"}); //res.json qaytaradi
 });
+
 
 app.get('/', function (req, res) {
     res.render("harid");
@@ -39,5 +41,5 @@ app.get('/', function (req, res) {
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function(){
-    console.log(`The server is running seccessfully on port:${{PORT}}`);
+    console.log(`The server is running seccessfully on port: ${{PORT}}`);
 });
