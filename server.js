47630942317a563d9f030e4c9e3e52dd4e8bb5f2
Khusan <khusan7700@gmail.com>
2024-02-07 13:40:07 +0500

    portfolia ejs

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -19,23 +30,27 @@ app.set("view engine", "ejs");
 // ejs orqali backend da fontend(HTML)ni yasaymiz.
 
 // 4 Ruting code
-app.get("/hello", function(req, res){
-    res.end(`<h1>HELLO WORLD</h1>`);
-});
+// app.get("/hello", function(req, res){
+//     res.end(`<h1>HELLO WORLD</h1>`);
+// });
 
-app.get("/gift", function(req, res){
-    res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
-});
-
-
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "seccess"}); //res.json qaytaradi
-});
+// app.get("/gift", function(req, res){
+//     res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
+// });
 
 
-app.get('/', function (req, res) {
-    res.render("harid");
+// app.post("/create-item", (req, res) => {
+//     console.log(req.body);
+//     res.json({test: "seccess"}); //res.json qaytaradi
+// });
+
+
+// app.get('/', function (req, res) {
+//     res.render("harid");
+// });
+
+app.get("/portfolia", (req, res) => {
+    res.render("portfolia", { user: user });
 });
 
 const server = http.createServer(app);
