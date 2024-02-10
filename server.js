38bd60bda2c30bc27034c83d2c6d84504a553513
Khusan <khusan7700@gmail.com>
2024-02-11 01:04:42 +0500

    feat: mondodb ulanish

--- server.js
+++ server.js
@@ -1,58 +1,32 @@
-console.log("Web serverni boshlash");
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
+const MongoDB = require("mongodb");
+console.log("mongo ishga tushdi");
+
+let db;
+const connectionString =
+  "mongodb+srv://Usman:7I1vQ04IkY8Mqs8w@cluster0.5e72uaw.mongodb.net/reja";
+
+MongoDB.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running seccessfully on port: ${PORT}, http//localhost:${PORT}`
+        );
+      });
+    }
   }
-});
-
-// 1 Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 2 Session code
-
-// 3 Views code
-//first neet to install EJS
-app.set("views", "views");
-app.set("view engine", "ejs");
-// ejs orqali backend da fontend(HTML)ni yasaymiz.
-
-// 4 Ruting code
-// app.get("/hello", function(req, res){
-//     res.end(`<h1>HELLO WORLD</h1>`);
-// });
-
-// app.get("/gift", function(req, res){
-//     res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
-// });
-
-// app.post("/create-item", (req, res) => {
-//     console.log(req.body);
-//     res.json({test: "seccess"}); //res.json qaytaradi
-// });
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-app.get("/portfolia", (req, res) => {
-  res.render("portfolia", { user: user });
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running seccessfully on port: ${PORT}, http//localhost:${PORT}`
-  );
-});
+);
